44ddfa963da86124bcbc50f9060cd8d593b2bb65
Ninjagoldfinch <88298491+Ninjagoldfinch@users.noreply.github.com>
2022-05-31 12:24:50 +1200

    Created Pistol
    Created shooting pistol

--- Godot Files/Scenes/EnemyBasicGrunt.gd
+++ Godot Files/Scenes/EnemyBasicGrunt.gd
@@ -11,7 +11,7 @@ var health = 20
 func _ready():
 	pass
 
-func take_damange(dmg_amount):
+func take_damage(dmg_amount):
 	health -= dmg_amount
 	if health <= 0:
 		death()
@@ -23,8 +23,8 @@ func _physics_process(delta):
 			path_index += 1
 		else:
 			move_and_slide(direction.normalized() * speed, Vector3.UP)
-	else:
-		find_path(player.global_transform.origin)
+
+	
 
 func find_path(target):
 	path = nav.get_simple_path(global_transform.origin,target)
@@ -41,3 +41,7 @@ func death():
 	
 func shoot(target):
 	pass
+
+
+func _on_Timer_timeout():
+	find_path(player.global_transform.origin)
