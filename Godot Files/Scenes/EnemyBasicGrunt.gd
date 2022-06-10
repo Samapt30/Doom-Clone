b20fa4f28ceae79a4a6cddd043993ee8e5c2e965
Ninjagoldfinch <88298491+Ninjagoldfinch@users.noreply.github.com>
2022-06-10 13:36:40 +1200

    Video 7
    Added Weapon Swap, and the SWAT shotgun.

--- Godot Files/Scenes/EnemyBasicGrunt.gd
+++ Godot Files/Scenes/EnemyBasicGrunt.gd
@@ -7,6 +7,7 @@ var path = [] # hold the path coordinates from the enemy to the player
 var path_index = 0 # keep track of which coordinate to go to
 var speed = 3
 var health = 20
+var move = true
 
 func _ready():
 	pass
@@ -15,6 +16,14 @@ func take_damage(dmg_amount):
 	health -= dmg_amount
 	if health <= 0:
 		death()
+		return
+
+	move = false
+	$AnimatedSprite3D.play("hit")
+	yield($AnimatedSprite3D,"animation_finished")	
+	move = true
+	
+		
 
 func _physics_process(delta):
 	if path_index < path.size():
@@ -22,9 +31,9 @@ func _physics_process(delta):
 		if direction.length() < 1:
 			path_index += 1
 		else:
-			move_and_slide(direction.normalized() * speed, Vector3.UP)
-
-	
+			if move:
+				$AnimatedSprite3D.play("walking")
+				move_and_slide(direction.normalized() * speed, Vector3.UP)
 
 func find_path(target):
 	path = nav.get_simple_path(global_transform.origin,target)
@@ -41,7 +50,6 @@ func death():
 	
 func shoot(target):
 	pass
-
 
 func _on_Timer_timeout():
 	find_path(player.global_transform.origin)
--- Godot Files/Scenes/Scripts/Player.gd
+++ Godot Files/Scenes/Scripts/Player.gd
@@ -7,6 +7,10 @@ var max_speed = 8
 var mouse_sensitivity = 0.002
 
 #Gun variables
+onready var pistol = preload("res://Scenes/Pistol.tscn")
+onready var shotgun = preload("res://Scenes/SWATShotgun.tscn")
+var current_gun = 0
+onready var carried_guns = [pistol,shotgun]
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -40,7 +44,18 @@ func _physics_process(delta):
 	
 
 func change_gun(gun):
-	pass
+	$Pivot/Gun.get_child(0).queue_free()
+	var new_gun = carried_guns[gun].instance()
+	$Pivot/Gun.add_child(new_gun)
 	
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("next_gun"):
+		current_gun += 1
+		if current_gun > len(carried_guns)-1:
+			current_gun = 0
+		change_gun(current_gun)
+	elif Input.is_action_just_pressed("prev_gun"):
+		current_gun -= 1
+		if current_gun < 0:
+			current_gun = len(carried_guns) - 1
+		change_gun(current_gun)
